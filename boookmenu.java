5271ad28a8648fd471a2b744b44664d8d887fe5d
moreee005 <162558278+moreee005@users.noreply.github.com>
2024-04-28 19:17:57 +0700

    Update boookmenu.java

--- boookmenu.java
+++ boookmenu.java
@@ -1,76 +1,51 @@
-private void displayRangeManga() {
-        List<Mangaka> mangaka = bookList.getMangaka();
+
+    private void displayRangeCountryAuthor() {
+        List<Book> books = bookList.getBooks();
         List<Publisher> publishers = bookList.getPublisher();
-        List<Comic> comics = bookList.getComic();
+        List<Author> authors = bookList.getAuthor();
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input Rating: ");
-        String ratingInput = scanner.nextLine();
+        System.out.println("Input Country: ");
+        String countryInput = scanner.nextLine();
 
-        List<Comic> booksBelowRange = new ArrayList<>(); // ArrayList untuk menyimpan buku di bawah rangePrice
+        List<Book> booksBelowRange = new ArrayList<>(); // ArrayList untuk menyimpan buku di bawah rangePrice
         List<Publisher> rangePublisher = new ArrayList<>(); //  ArrayList untuk menyimpan production cost buku di bawah rangePrice
-        List<Mangaka> mangakaBelowRange = new ArrayList<>();
-        for (Publisher publisher : publishers) {
-            for (Comic comic1 : comics) {
-                for (Mangaka mangaka1 : mangaka) {
-                    String publisher1 = publisher.getName();
-                    String firstName = mangaka1.getFirstName();
-                    String lastName = mangaka1.getLastName();
-                    String name = firstName + lastName;
-                    String author1 = comic1.getAuthor();
+        List<Author> authorsBelowRange = new ArrayList<>();
+        for (Author author : authors) {
+            if (countryInput.equals(author.getCountry())) {
+                authorsBelowRange.add(author);
+            }
+        }
 
-                    // System.out.println(author1);
-                    // System.out.println(publisher1);
-                    //System.out.println(name);
-                    if (name.equals(comic1.getAuthor())) {
-                        booksBelowRange.add(comic1); // Menambahkan buku ke dalam ArrayList
-                        rangePublisher.add(publisher);
-                        if (comic1.getPublisher().equalsIgnoreCase(publisher.getName())) {
-
-                            double productionCost = publisher.getProductionCost();
-                            double price = 0.0;
-                            if (mangaka1.getRating().equals("New Commer")) {
-                                if (comic1.getSeries()) {
-                                    price = productionCost * 1.35;
-
-                                } else {
-                                    price = productionCost * 1.25;
-                                }
-                            } else if (mangaka1.getRating().equals("Good")) {
-                                if (comic1.getSeries()) {
-                                    price = productionCost * 1.45;
-                                } else {
-                                    price = productionCost * 1.3;
-                                }
-                            } else if (mangaka1.getRating().equals("Best Seller")) {
-                                if (comic1.getSeries()) {
-                                    price = productionCost * 1.5;
-                                } else {
-                                    price = productionCost * 1.4;
-                                }
-                            }
-
-                            if (price > 0) {
-                                if (ratingInput.equals(mangaka1.getRating())) {
-                                    booksBelowRange.add(comic1);
-                                    rangePublisher.add(publisher);
-                                    mangakaBelowRange.add(mangaka1);
-                                }
-                            }
-                            System.out.println("Code : " + comic1.getCode());
-                            System.out.println("Title : " + comic1.getTitle());
-                            System.out.println("Author : " + comic1.getAuthor());
-                            System.out.println("Publisher : " + comic1.getPublisher());
-                            System.out.println("Series : " + comic1.getSeries());
-                            System.out.println("Price: " + price);
-                            System.out.println("Rating :" + mangaka1.getRating());
-                            System.out.println();
-
-                        }
-
-                    }
-
+//        Get comic
+        for (Author author : authorsBelowRange) {
+            String firstName = author.getFirstName();
+            String lastName = author.getLastName();
+            String name = firstName + lastName;
+            for (Book book : books) {
+                if (book.getAuthor().equals(name)) {
+                    booksBelowRange.add(book);
                 }
             }
+        }
 
+        for (Author author : authorsBelowRange) {
+            for (Book book : booksBelowRange) {
+                for (Publisher publisher : publishers) {
+                    if (book.getPublisher().equals(publisher.getName())) {
+                        rangePublisher.add(publisher);
+                        double price = publisher.getProductionCost() * 1.2;
+                        System.out.println("Code : " + book.getCode());
+                        System.out.println("Title : " + book.getTitle());
+                        System.out.println("Author : " + book.getAuthor());
+                        System.out.println("Publisher : " + book.getPublisher());
+                        System.out.println("Price: " + price);
+                        System.out.println("Country :" + author.getCountry());
+                        System.out.println();
+                        break;
+                    }
+                }
+            }
         }
     }
+}
